020fcdd4cf89e21334881d40a402da33705f6075
PXY <850396118@qq.com>
2020-05-05 15:15:19 +0800

    Exercise1.1.2

---
+++ src/chapter1_1/Example_02.java
@@ -0,0 +1,12 @@
+package chapter1_1;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class Example_02 {
+    public static void main(String[] args) {
+        System.out.println((1 + 2.236) / 2);    //1.618 double
+        System.out.println(1 + 2 + 3 + 4.0);    //10.0 double
+        System.out.println(4.1 >= 4);           //true boolean
+        System.out.println(1 + 2 + "3");        //33  String
+    }
+}
