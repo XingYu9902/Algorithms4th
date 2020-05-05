7972cc8dbecf696a4894179c3185e9b43deb3fc1
PXY <850396118@qq.com>
2020-05-05 16:11:37 +0800

    Exercise1.1.8

---
+++ src/chapter1_1/Example_08.java
@@ -0,0 +1,17 @@
+package chapter1_1;
+
+public class Example_08 {
+    public static void main(String[] args) {
+        System.out.println('b');
+        System.out.println('b'+'c');
+        System.out.println(('a'+4));
+        System.out.println((char)('a'+4));
+    }
+    /*
+        运行结果：
+                b
+                197
+                101
+                e
+     */
+}
