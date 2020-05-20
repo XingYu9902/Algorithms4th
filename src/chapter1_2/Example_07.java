6f6fda71fbbba44cad16102bc259e9743c101f56
PXY <850396118@qq.com>
2020-05-20 10:50:47 +0800

    Exercise1.2.7

---
+++ src/chapter1_2/Example_07.java
@@ -0,0 +1,22 @@
+package chapter1_2;
+
+import edu.princeton.cs.algs4.Transaction;
+
+/*
+    字符串的翻转
+ */
+public class Example_07 {
+    public static String mystery(String s){
+        int N = s.length();
+        if (N<=1){
+            return s;
+        }
+        String a = s.substring(0,N/2);
+        String b = s.substring(N/2,N);
+        return mystery(b)+mystery(a);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mystery("abc bca asd"));
+    }
+}
